f46bf2a86c826c5ef5a346258682b61c7283543c
k-kotera <04mwmr6c@gmail.com>
2025-08-11 19:10:43 +0900

    コミット

--- public/src/ui/shared.js
+++ public/src/ui/shared.js
@@ -1,3 +1,8 @@
 import { state, save } from '../state.js';
-import { renderAll } from './render.js';
-export function afterPick(){ state.gold += 1; save(); renderAll(); }
+import { renderBrief } from './render.js';  // ← renderAll ではなく renderBrief を使う
+
+export function afterPick(){
+  state.gold += 1;
+  save();
+  renderBrief();  // 画面はブリーフィングなのでこちらを再描画
+}
